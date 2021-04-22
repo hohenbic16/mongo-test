84d0b5e1048e3f74c02dca0abd9061b6bf7ff7ed
hohenbic16 <48672017+hohenbic16@users.noreply.github.com>
2021-04-22 11:58:06 +0200

    Update server.js

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ mongoose.connect(dbConfig.url, {
 
 // define a simple route
 app.get('/', (req, res) => {
-    res.json({"message": "Welcome to the kitty db."});
+    res.json({"message": "Welcome to the fabulous kitty db."});
 });
 
 require('./app/routes/cat.routes.js')(app);
