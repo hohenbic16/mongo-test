769e8f62eab954cc22a52f6526c26ed137ebd1c8
hohenbic16 <48672017+hohenbic16@users.noreply.github.com>
2021-04-22 14:04:02 +0200

    Update cat.model.js

--- app/models/cat.model.js
+++ app/models/cat.model.js
@@ -3,7 +3,9 @@ const mongoose = require('mongoose');
 const CatSchema = mongoose.Schema({
     name: String,
     owner: String,
-    color: String
+    color: String, 
+    unique_sound: String, 
+    superpower: String
 }, {
     timestamps: true
 });
